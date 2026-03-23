3b9e80e3529633d00b869ce4e6ec2f2fd60b96c7
Ramiro Usnayo <rusnayo22@gmail.com>
2026-03-23 14:09:05 -0300

    feat: bonus - CRUD consultas, página de inicio y mensajes de éxito

--- fichas/urls.py
+++ fichas/urls.py
@@ -17,4 +17,14 @@ urlpatterns = [
     path('mascotas/nuevo/', views.MascotaCreateView.as_view(), name='mascota_crear'),
     path('mascotas/editar/<int:pk>/', views.MascotaUpdateView.as_view(), name='mascota_editar'),
     path('mascotas/eliminar/<int:pk>/', views.MascotaDeleteView.as_view(), name='mascota_eliminar'),
+
+    # Consultas Médicas
+    path('consultas/', views.ConsultaListView.as_view(), name='consulta_lista'),
+    path('consultas/<int:pk>/', views.ConsultaDetailView.as_view(), name='consulta_detalle'),
+    path('consultas/nueva/', views.ConsultaCreateView.as_view(), name='consulta_crear'),
+    path('consultas/editar/<int:pk>/', views.ConsultaUpdateView.as_view(), name='consulta_editar'),
+    path('consultas/eliminar/<int:pk>/', views.ConsultaDeleteView.as_view(), name='consulta_eliminar'),
+
+    # Inicio
+    path('', views.inicio, name='inicio'),
 ]
